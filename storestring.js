8af2d9b06ebb10039967a08e397ab3fbbe5304d3
awasthilokesh2021 <115865786+awasthilokesh2021@users.noreply.github.com>
2023-08-14 17:49:24 +0530

    Add files via upload

---
+++ storestring.js
@@ -0,0 +1,13 @@
+// Objects and Arrays
+
+const players = ["John", "Bob", "Alice", "Poppy"];
+const person = {
+  name: "John Doe",
+  age: 80,
+};
+
+const team1 = [...players]
+const cap1 = {...person}
+
+console.log(cap1)
+console.log(team1)
